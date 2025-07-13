9e584ec162b6337b406fafddb9b97390ad21665a
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-07-13 17:18:49 -0600

    scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import Lenis from 'lenis';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
   animationState: 'idle' | 'loading' | 'loaded' = 'idle';
   playAnimation = true;
+  private lenis: Lenis | null = null;
+  private rafHandle: number | null = null;
+
+  @ViewChild('cuerpo') cuerpo!: ElementRef<HTMLDivElement>;
 
   private initialTimer: any;
   private loadingTimer: any;
@@ -21,15 +26,49 @@ export class HomeComponent implements OnInit, OnDestroy {
 
         this.loadingTimer = setTimeout(() => {
           this.animationState = 'loaded';
+          this.onAnimationEnd();
         }, 5000);
       }, 0);
     } else {
       this.animationState = 'loaded';
+      this.onAnimationEnd();
     }
   }
 
   ngOnDestroy() {
     clearTimeout(this.initialTimer);
     clearTimeout(this.loadingTimer);
+    if (this.rafHandle) {
+      cancelAnimationFrame(this.rafHandle);
+    }
+    this.lenis?.destroy();
+  }
+
+  ngAfterViewInit() {
+    this.lenis = new Lenis({
+      wrapper: this.cuerpo.nativeElement,
+    });
+
+    this.lenis.stop();
+
+    const raf = (time: number) => {
+      this.lenis?.raf(time);
+      this.rafHandle = requestAnimationFrame(raf);
+    };
+
+    this.rafHandle = requestAnimationFrame(raf);
+  }
+
+  onAnimationEnd() {
+    const subtitulo = document.querySelector('.subtitulo') as HTMLElement;
+
+    setTimeout(() => {
+      subtitulo.classList.add('visible');
+      setTimeout(() => {
+        this.cuerpo.nativeElement.classList.add('scroll');
+        this.lenis?.start();
+        console.log('scrolito started');
+      }, 500);
+    }, 1500);
   }
 }
