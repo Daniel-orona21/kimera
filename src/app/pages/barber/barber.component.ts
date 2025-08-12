504f8a4526198cef8ea7105d2ad67e7085967db8
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-08-12 10:33:53 -0600

    phone numbers

--- src/app/pages/barber/barber.component.ts
+++ src/app/pages/barber/barber.component.ts
@@ -9,4 +9,25 @@ import { TextRevealSimpleComponent } from '../../shared/components/text-reveal/t
   styleUrl: './barber.component.scss'
 })
 export class BarberComponent {
+
+  public whatsApp(barbero: string) {
+    let url = '';
+  
+    switch (barbero) {
+      case 'yahir':
+        url = 'https://wa.me/6183286528?text=Hola!'; // Número de Yahir
+        break;
+      case 'leo':
+        url = 'https://wa.me/6182955673?text=Hola!'; // Número de Leo
+        break;
+      case 'erick':
+        url = 'https://wa.me/6181349978?text=Hola!'; // Número de Erick
+        break;
+      default:
+        console.warn('Barbero no encontrado');
+        return;
+    }
+  
+    window.open(url, '_blank');
+  }
 }
